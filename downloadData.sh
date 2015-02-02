5cfa698a778395fac680150ad85104329c4fd42c
wacax <wacax@hotmail.com>
2015-02-02 15:49:37 -0500

    data download ready

---
+++ downloadData.sh
@@ -0,0 +1,9 @@
+wget -x --load-cookies cookies.txt -P Data -nH --cut-dirs=5 http://www.kaggle.com/c/march-machine-learning-mania-2015/download/tourney_compact_results.csv
+wget -x --load-cookies cookies.txt -P Data -nH --cut-dirs=5 http://www.kaggle.com/c/march-machine-learning-mania-2015/download/seasons.csv
+wget -x --load-cookies cookies.txt -P Data -nH --cut-dirs=5 http://www.kaggle.com/c/march-machine-learning-mania-2015/download/teams.csv
+wget -x --load-cookies cookies.txt -P Data -nH --cut-dirs=5 http://www.kaggle.com/c/march-machine-learning-mania-2015/download/tourney_detailed_results.csv
+wget -x --load-cookies cookies.txt -P Data -nH --cut-dirs=5 http://www.kaggle.com/c/march-machine-learning-mania-2015/download/tourney_slots.csv
+wget -x --load-cookies cookies.txt -P Data -nH --cut-dirs=5 http://www.kaggle.com/c/march-machine-learning-mania-2015/download/tourney_seeds.csv
+wget -x --load-cookies cookies.txt -P Data -nH --cut-dirs=5 http://www.kaggle.com/c/march-machine-learning-mania-2015/download/regular_season_detailed_results.csv
+wget -x --load-cookies cookies.txt -P Data -nH --cut-dirs=5 http://www.kaggle.com/c/march-machine-learning-mania-2015/download/regular_season_compact_results.csv
+wget -x --load-cookies cookies.txt -P Data -nH --cut-dirs=5 http://www.kaggle.com/c/march-machine-learning-mania-2015/download/sample_submission.csv
